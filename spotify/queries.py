1a52e8dd3fbb8ab9bfed041da3b0da1ce45961be
Matthew Faw <matthewfaw@gmail.com>
2017-06-10 16:11:08 -0400

    Makes the Spotify query script slightly more general

--- spotify/queries.py
+++ spotify/queries.py
@@ -9,7 +9,7 @@ SPOTIFY_CLIENT_SECRET = os.environ['spotify_client_secret']
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
 s = Spotify(client_credentials_manager=client_credentials_manager)
 
-def get_genres(query):
-    return s.search(q=query, type='artist')['artists']['items'][0]['genres']
+def get_genres(query, qtype='artist'):
+    return s.search(q=query, type=qtype)[qtype+'s']['items'][0]['genres']
 
 # pprint(get_genres('Kanye West'))
